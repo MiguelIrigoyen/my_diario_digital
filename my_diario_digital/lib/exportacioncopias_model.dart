8c756c3c85de61008b9aadf65b2b7776de7be952
MiguelIrigoyen <21041550@itdurango.edu.mx>
2024-11-27 10:30:34 -0600

    paquete agregado

---
+++ my_diario_digital/lib/exportacioncopias_model.dart
@@ -0,0 +1,20 @@
+import '/flutter_flow/flutter_flow_theme.dart';
+import '/flutter_flow/flutter_flow_util.dart';
+import '/flutter_flow/flutter_flow_widgets.dart';
+import 'exportacioncopias_widget.dart' show ExportacioncopiasWidget;
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+
+class ExportacioncopiasModel extends FlutterFlowModel<ExportacioncopiasWidget> {
+  ///  State fields for stateful widgets in this page.
+
+  // State field(s) for copiaSeguriadadCheckbox widget.
+  bool? copiaSeguriadadCheckboxValue;
+
+  @override
+  void initState(BuildContext context) {}
+
+  @override
+  void dispose() {}
+}
