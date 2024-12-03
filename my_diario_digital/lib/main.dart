d8f6a84a82294918d6fff371f4de82b23783cf82
sear0997 <21041399@itdurango.edu.mx>
2024-12-02 23:46:25 -0600

    Actualizacion del main.dart
    Se añadio el codigo que hace referencia a las pantallas de Calendario y Configuracion

--- my_diario_digital/lib/main.dart
+++ my_diario_digital/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-//import 'calendario.dart';
-//import 'configuracion.dart';
+import 'calendario.dart';
+import 'configuracion.dart';
 import 'detallesentrada.dart';
 // 'entradas.dart';
 //import 'etiquetas.dart';
@@ -23,6 +23,8 @@ class MyApp extends StatelessWidget {
         '/entradas':  (context) =>CrearEntrada() ,
         '/ecSeguridad': (context) => ExportarCopiaSeguridad(),
         '/Inicio': (context) => PantallaPrincipal(),
+        '/Calendario': (context) => Calendario(),
+        '/Configuracion': (context) => Configuracion(),
       },
     );
   }
@@ -34,51 +36,58 @@ class PantallaPrincipal extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.blue,
-        title:Center(
-        child: Text('Pantalla Principal',
-
-               style: TextStyle(color: Colors.white),
-        ),
+        title: Center(
+          child: Text(
+            'Pantalla Principal',
+            style: TextStyle(color: Colors.white),
+          ),
         ),
       ),
-    drawer: Drawer(
-    child: ListView(
-    padding: EdgeInsets.zero,
-    children: [
-    DrawerHeader(
-    decoration: BoxDecoration(color: Colors.blue),
-    child: Text(
-    'Menú',
-    style: TextStyle(
-    color: Colors.white,
-    fontSize: 24,
-    ),
-    ),
-    ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            DrawerHeader(
+              decoration: BoxDecoration(color: Colors.blue),
+              child: Text(
+                'Menú',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
+                ),
+              ),
+            ),
     ListTile(
-    leading: Icon(Icons.home),
-    title: Text('Inicio'),
-    onTap: () {
-      Navigator.pushNamed(context, '/Inicio');
-    },
-    ),
+              leading: Icon(Icons.home),
+              title: Text('Inicio'),
+              onTap: () {
+                Navigator.pushNamed(context, '/Inicio');
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.settings),
+              title: Text('Configuración'),
+              onTap: () {
+                Navigator.pushNamed(context, '/Configuracion');
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.calendar_today),
+              title: Text('Calendario'),
+              onTap: () {
+                Navigator.pushNamed(context, '/Calendario');
+              },
+            ),
     ListTile(
-    leading: Icon(Icons.settings),
-    title: Text('Configuración'),
-    onTap: () {
-    // Acción al presionar
-    },
-    ),
-    ListTile(
-    leading: Icon(Icons.logout),
-    title: Text('Cerrar sesión'),
-    onTap: () {
-    // Acción al presionar
-    },
-    ),
-    ],
-    ),
-    ),
+              leading: Icon(Icons.logout),
+              title: Text('Cerrar sesión'),
+              onTap: () {
+                // Acción al presionar
+              },
+            ),
+          ],
+        ),
+      ),
       body: Center(
         child: ElevatedButton(
           onPressed: () {
@@ -103,22 +112,131 @@ class PantallaPrincipal extends StatelessWidget {
             IconButton(
               icon: Icon(Icons.archive_rounded),
               onPressed: () {
-                // Navegar a la pantalla de exportacion  y copia de seguridad
                 Navigator.pushNamed(context, '/ecSeguridad');
               },
             ),
             IconButton(
               icon: Icon(Icons.add_box_outlined),
               onPressed: () {
-                // Navegar a la entradas
                 Navigator.pushNamed(context, '/entradas');
               },
             ),
             IconButton(
               icon: Icon(Icons.access_alarm),
               onPressed: () {
-                // Navegar a la entradas
                 Navigator.pushNamed(context, '/entradas');
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Calendario extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Calendario'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Calendario de Entradas'),
+            ElevatedButton(
+              onPressed: () {
+                // Lógica para obtener entradas por fecha
+                print('Obteniendo entradas por fecha');
+              },
+              child: Text('Obtener Entradas'),
+            ),
+          ],
+        ),
+      ),
+       bottomNavigationBar: BottomAppBar(
+        color: Colors.blue,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            IconButton(
+              icon: Icon(Icons.home),
+              onPressed: () {
+                Navigator.pushNamed(context, '/Inicio');
+              },
+            ),
+            IconButton(
+              icon: Icon(Icons.archive_rounded),
+              onPressed: () {
+                Navigator.pushNamed(context, '/ecSeguridad');
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Configuracion extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Configuración'),
+      ),
+      body: ListView(
+        padding: EdgeInsets.all(16.0),
+        children: [
+          ListTile(
+            leading: Icon(Icons.camera_alt),
+            title: Text('Configurar permisos de cámara'),
+            trailing: Switch(
+              value: true,
+              onChanged: (value) {
+                // Lógica para actualizar permiso
+                print('Permiso de cámara actualizado: $value');
+              },
+            ),
+          ),
+          ListTile(
+            leading: Icon(Icons.color_lens),
+            title: Text('Cambiar tema'),
+            onTap: () {
+              // Lógica para cambiar tema
+              print('Cambiando tema');
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.notifications),
+            title: Text('Ajustar notificaciones'),
+            trailing: Switch(
+              value: false,
+              onChanged: (value) {
+                // Lógica para ajustar notificaciones
+                print('Notificaciones actualizadas: $value');
+              },
+            ),
+          ),
+        ],
+      ),
+      bottomNavigationBar: BottomAppBar(
+        color: Colors.blue,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            IconButton(
+              icon: Icon(Icons.home),
+              onPressed: () {
+                Navigator.pushNamed(context, '/Inicio');
+              },
+            ),
+            IconButton(
+              icon: Icon(Icons.archive_rounded),
+              onPressed: () {
+                Navigator.pushNamed(context, '/ecSeguridad');
               },
             ),
           ],
