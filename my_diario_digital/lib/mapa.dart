1b67832064ac3cf28ef65a1a697b7ffdcf16223f
EdgaRojas <21041411@itdurango.edu.mx>
2024-12-04 10:45:41 -0600

    a

--- my_diario_digital/lib/mapa.dart
+++ my_diario_digital/lib/mapa.dart
@@ -8,25 +8,11 @@ class GoogleMapPage extends StatefulWidget {
 
 class _GoogleMapPageState extends State<GoogleMapPage> {
   late GoogleMapController _controller;
-  final Set<Marker> _markers = {}; // Use a Set for markers
 
   final CameraPosition _initialPosition = CameraPosition(
-    target: LatLng(24.02032, -104.66756), // Victoria de Durango
-    zoom: 14, // Increased initial zoom for better visibility
+    target: LatLng(24.02032, -104.66756), //Victoria de durango
+    zoom: 10,
   );
-
-  @override
-  void initState() {
-    super.initState();
-    // Initialize markers in initState
-    _markers.add(
-      Marker(
-        markerId: MarkerId('mk1'),
-        position: LatLng(24.03121944, -104.64691389), // ITD coordinates
-        infoWindow: InfoWindow(title: 'El Tecno'),
-      ),
-    );
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -39,7 +25,13 @@ class _GoogleMapPageState extends State<GoogleMapPage> {
         onMapCreated: (controller) {
           _controller = controller;
         },
-        markers: _markers, // Use the _markers Set
+        markers: {
+          Marker(
+            markerId: MarkerId('mk1'),
+            position: LatLng(24.03121944, -104.64691389), //coordenadas del ITD
+            infoWindow: InfoWindow(title: 'El tecno'),
+          ),
+        },
       ),
     );
   }
