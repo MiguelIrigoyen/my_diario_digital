425fd067a529025daeaf2dad177c86d45c2c5d63
MiguelIrigoyen <21041550@itdurango.edu.mx>
2024-12-03 19:45:45 -0600

    a

--- my_diario_digital/lib/mapa.dart
+++ my_diario_digital/lib/mapa.dart
@@ -1,1 +1,38 @@
-hola
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class GoogleMapPage extends StatefulWidget {
+  @override
+  _GoogleMapPageState createState() => _GoogleMapPageState();
+}
+
+class _GoogleMapPageState extends State<GoogleMapPage> {
+  late GoogleMapController _controller;
+
+  final CameraPosition _initialPosition = CameraPosition(
+    target: LatLng(24.02032, -104.66756), //Victoria de durango
+    zoom: 10,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Google Maps en Flutter'),
+      ),
+      body: GoogleMap(
+        initialCameraPosition: _initialPosition,
+        onMapCreated: (controller) {
+          _controller = controller;
+        },
+        markers: {
+          Marker(
+            markerId: MarkerId('mk1'),
+            position: LatLng(24.03121944, -104.64691389), //coordenadas del ITD
+            infoWindow: InfoWindow(title: 'El tecno'),
+          ),
+        },
+      ),
+    );
+  }
+}
